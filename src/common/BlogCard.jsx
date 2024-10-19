96f2ac0cbf47f3b3166d1262236a6705e08d428f
sayan.pro.id@gmail.com <sayan.pramanick@qubrid.com>
2024-10-19 22:58:00 +0530

    link fixed

--- src/common/BlogCard.jsx
+++ src/common/BlogCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BlogCard = ({ src, title }) => {
     return (
-        <a  href="" target='_blank' rel="noreferrer">
+        <a style={{ border: "1px solid white" }} href="https://medium.com/@sayanpramanick/optimizing-react-performance-with-memoization-264e0bfe6ac6" target='_blank' rel="noreferrer">
             <img className='hover'
                 src={src}
                 alt={`blog logo`}

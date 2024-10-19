0a73ac33d9ee1c72bed76bb94dab10addbdb976d
sayan.pro.id@gmail.com <sayan.pramanick@qubrid.com>
2024-10-20 03:56:09 +0530

    scroll fixed in blogs

--- src/sections/Blogs/Blogs.jsx
+++ src/sections/Blogs/Blogs.jsx
@@ -1,32 +1,53 @@
-import styles from './BlogsStyles.module.css'
-import { blogList } from '../../utils/blogData'
-
+import { useEffect, useRef } from 'react';
+import styles from './BlogsStyles.module.css';
+import { blogList } from '../../utils/blogData';
+import { FaAnglesLeft, FaAnglesRight } from 'react-icons/fa6';
 
 const Blogs = () => {
-    return (
-        <section id='blogs' className={styles.container}>
-            <h1 className='sectionTitle'>Blogs</h1>
-            <div className={styles.blogsContainer}>
+    const blogsContainerRef = useRef(null);
 
-                {blogList.map((item, key) => {
-                    return (
-                        <a key={item.id + key}
-                            href={item.link} target='_blank' rel="noreferrer">
-                            <img className='hover'
-                                src={item.image}
-                                alt={`blog logo`}
-                            />
+    const scrollLeft = () => {
+        if (blogsContainerRef.current) {
+            blogsContainerRef.current.scrollLeft -= 300;
+        }
+    };
+
+    const scrollRight = () => {
+        if (blogsContainerRef.current) {
+            blogsContainerRef.current.scrollLeft += 300;
+        }
+    };
+
+    useEffect(() => {
+        setInterval(() => {
+            if (blogsContainerRef.current) {
+                blogsContainerRef.current.scrollLeft += 300;
+            }
+        }, 10000);
+
+
+    }, [])
+
+
+    return (
+        <section id="blogs" className={styles.container}>
+            <h1 className="sectionTitle">Blogs</h1>
+            <div className={styles.slider}>
+
+                <FaAnglesLeft onClick={scrollLeft} className={styles.scrollButtonLeft} />
+                <div className={styles.blogsContainer} ref={blogsContainerRef}>
+                    {blogList.map((item, key) => (
+                        <a key={item.id + key} href={item.link} target="_blank" rel="noreferrer">
+                            <img className="hover" src={item.image} alt={`blog logo`} />
                             <p>{item.title}</p>
                         </a>
-                    )
-                })
-
-                }
+                    ))}
+                </div>
+                <FaAnglesRight onClick={scrollRight} className={styles.scrollButtonRight} />
 
             </div>
-
         </section>
-    )
-}
+    );
+};
 
-export default Blogs
+export default Blogs;
